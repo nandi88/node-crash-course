78672d0b79a9464fcd28affaf2bf3d0f29e6c133
Nandi Stapelberg <nandi.stapelberg671@gmail.com>
2020-06-26 09:01:29 +0200

    Lesson 2 stream and file

--- streams.js
+++ streams.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/stream.txt');
+const readStream = fs.createReadStream('./docs/stream.txt', { encoding: 'utf8' });
+const writeStream = fs.createWriteStream('./docs/writeStream.txt');
 
 
 //eventlistener
 //data gets a chuck of data and every time you get a chunk the chunk callback fires
-readStream.on('data', (chunk) => {
-    console.log('--------New chunk---------');
-    console.log(chunk);
-});
+// readStream.on('data', (chunk) => {
+//     console.log('--------New chunk---------');
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+
+// });
+
+readStream.pipe(writeStream);
