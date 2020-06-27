ff3d0f2e44e5c13f1642a15831b36f6ef9cacf3e
Nandi Stapelberg <nandi.stapelberg671@gmail.com>
2020-06-27 14:44:00 +0200

    Mogoose
     models, views, partials, public folders. EJS, mongoose, sandbox routes to show how to get and create data from mongoose

--- app.js
+++ app.js
@@ -1,31 +1,74 @@
 //create express function
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //set up express app and invoke express and stor in app const
 const app = express();
 
+//connect to mongodb
+const bdURI = 'mongodb+srv://netninja:scientist@nodetuts-6iczn.mongodb.net/node-tuts?retryWrites=true&w=majority';
+
+//deprecation warning so add 2nd parameter
+mongoose.connect(bdURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
+
 //set up view engine
 app.set('view engine', 'ejs');
 
-//listen for requests
-app.listen(3000);
+//middleware on static files
+app.use(express.static('public'));
+//you can just invoke the function
+app.use(morgan('dev'));
 
-//listen for get request
-app.get('/', (req, res) => {
-    res.render('index');
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    //create new instance of the blog model
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippets: 'about my new blog',
+        body: 'lorem ipsum dolarjhvsvhjkfzsshjkfsahksfah fhfghgfhukfghk'
+    });
+
+    blog.save()
+        .then(result => res.send(result))
+        .catch(err => console.log(err));
 });
 
-// app.get('/about', (req, res) => {
-//     res.sendfile('./views/about.html', {root: __dirname});
-// });
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then(result => res.send(result))
+        .catch(err => console.log(err));
+});
 
-// //redirects 
-// app.get('/about-me', (res, req) => {
-//     res.redirect('/about');
-// });
+app.get('/single-blog', (req, res) => {
+    Blog.findById('5ef738b687379e0b94a82867')
+        .then(result => res.send(result))
+        .catch(err => console.log(err));
+});
 
-// //404 must be last because of use()
-// //use() method will then check every get handler to check if there is a url that matches
-// app.use((req, res) => {
-//     res.status(404).sendfile('./views/404.html', {root: __dirname});
-// });
+//listen for get request
+//render view
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'about' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'create new blog' });
+});
+
+//404 must be last
+app.use((req, res) => {
+    res.status(404).render('404' , { title: '404' });
+});
---
+++ models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+//get something fom mongoose object, so create Schema
+const Schema = mongoose.Schema;
+
+//create new instance of Schema object
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippets: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps:true });
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
