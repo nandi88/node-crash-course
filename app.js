939b5922df2e4443d963ca9368998bf9228712a9
Nandi Stapelberg <nandi.stapelberg671@gmail.com>
2020-06-27 09:25:11 +0200

    Changes

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ app.listen(3000);
 
 //listen for get request
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 });
 
-app.get('/about', (req, res) => {
-    res.sendfile('./views/about.html', {root: __dirname});
-});
+// app.get('/about', (req, res) => {
+//     res.sendfile('./views/about.html', {root: __dirname});
+// });
 
-//redirects 
-app.get('/about-me', (res, req) => {
-    res.redirect('/about');
-});
+// //redirects 
+// app.get('/about-me', (res, req) => {
+//     res.redirect('/about');
+// });
 
-//404 must be last because of use()
-//use() method will then check every get handler to check if there is a url that matches
-app.use((req, res) => {
-    res.status(404).sendfile('./views/404.html', {root: __dirname});
-});
+// //404 must be last because of use()
+// //use() method will then check every get handler to check if there is a url that matches
+// app.use((req, res) => {
+//     res.status(404).sendfile('./views/404.html', {root: __dirname});
+// });
--- server.js
+++ server.js
@@ -1,57 +1,57 @@
-const http = require('http');
-const fs = require('fs');
-const _ = require('lodash');
+// const http = require('http');
+// const fs = require('fs');
+// const _ = require('lodash');
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-  //lodash
-  const num = _.random(0, 20);
-  console.log(num);
+//   //lodash
+//   const num = _.random(0, 20);
+//   console.log(num);
 
-  const greet = _.once(() => {
-    console.log('hello');
-  });
+//   const greet = _.once(() => {
+//     console.log('hello');
+//   });
 
-  greet();
+//   greet();
 
-  //set header content type
-  res.setHeader('Content-Type', 'text/html');
+//   //set header content type
+//   res.setHeader('Content-Type', 'text/html');
 
-  //routing
-  let path = './views/';
-  switch(req.url) {
-    case '/':
-      path += 'index.html';
-      res.statusCode = 200;
-      break;
-    case '/about':
-      path += 'about.html';
-      res.statusCode = 200;
-      break;
-    case '/about-me':
-      res.statusCode = 301;
-      res.setHeader('Location', '/about');
-      res.end();
-      break;
-    default:
-      path += '404.html';
-      res.statusCode = 404;
-      break;
-  }
+//   //routing
+//   let path = './views/';
+//   switch(req.url) {
+//     case '/':
+//       path += 'index.html';
+//       res.statusCode = 200;
+//       break;
+//     case '/about':
+//       path += 'about.html';
+//       res.statusCode = 200;
+//       break;
+//     case '/about-me':
+//       res.statusCode = 301;
+//       res.setHeader('Location', '/about');
+//       res.end();
+//       break;
+//     default:
+//       path += '404.html';
+//       res.statusCode = 404;
+//       break;
+//   }
 
-  //read file to send back data of file as response to browser
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      //add end method too in error block
-      res.end();
-    } else {
-      res.end(data);
-    }
-  });
+//   //read file to send back data of file as response to browser
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       //add end method too in error block
+//       res.end();
+//     } else {
+//       res.end(data);
+//     }
+//   });
 
-});
+// });
 
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
-});
+// server.listen(3000, 'localhost', () => {
+//   console.log('listening for requests on port 3000');
+// });
