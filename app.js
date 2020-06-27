ccd24e1d96c81929bba74f6ff690a36b92dcfe49
Nandi Stapelberg <nandi.stapelberg671@gmail.com>
2020-06-27 09:03:33 +0200

    Ninja node course

---
+++ app.js
@@ -0,0 +1,31 @@
+//create express function
+const express = require('express');
+
+//set up express app and invoke express and stor in app const
+const app = express();
+
+//set up view engine
+app.set('view engine', 'ejs');
+
+//listen for requests
+app.listen(3000);
+
+//listen for get request
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    res.sendfile('./views/about.html', {root: __dirname});
+});
+
+//redirects 
+app.get('/about-me', (res, req) => {
+    res.redirect('/about');
+});
+
+//404 must be last because of use()
+//use() method will then check every get handler to check if there is a url that matches
+app.use((req, res) => {
+    res.status(404).sendfile('./views/404.html', {root: __dirname});
+});
